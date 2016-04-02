507d89b349c321fc6700ca27f5d60f5fb1eb7c73
apurvajajodia <apurva.jajodia@yahoo.com>
2016-04-02 22:47:46 +0530

    commiting transaction.rb

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -9,11 +9,34 @@ class Transaction
 	def initialize(name, product)
 		@customer = name
 		@product = product
-		@@transactions << self
-		@@count = @@count + 1
-		@id = @@count
-		@product.stock = @product.stock - 1
+		make_purchase
 
+	end
+
+	def self.all
+		@@transactions
+	end
+
+	def self.find(id_no)
+		@@transactions.each do |i|
+			if(i.id == id_no)
+			puts "found it!" #This is so that the user can know that the program found what he was looking for
+			return i
+			end
+		end
+	end	
+
+	private
+	def make_purchase
+		if (@product.stock > 0) then
+			@@transactions << self
+			@@count = @@count + 1
+			@id = @@count
+			@product.stock = @product.stock - 1
+			puts "inside the purchase if"
+		else
+			raise OutOfStockError, "#{@product.title} is out of stock"
+		end
 	end
 
 
