0a17d6c238180208443aeebb969efdfb64de348b
apurvajajodia <apurva.jajodia@yahoo.com>
2016-04-03 12:22:25 +0530

    added new features

--- lib/product.rb
+++ lib/product.rb
@@ -38,7 +38,27 @@ class Product
 				@@product_in_stock.push(i)
 			end
 		end
-		return @@product_in_stock
+		@@product_in_stock
+	end
+
+	def self.out_stock
+		@@products.each do |i|
+			if(i.stock == 0)
+				puts "The below items are out of stock, please consider replenishing"
+				puts "#{i.title}"
+			end
+		end
+		
+	end
+
+	def self.low_stock
+		@@products.each do |i|
+			if(i.stock >0 && i.stock < 5)
+				puts "The below items are low on stock, please consider replenishing"
+				puts "#{i.title}"
+			end
+		end
+		
 	end
 
 
